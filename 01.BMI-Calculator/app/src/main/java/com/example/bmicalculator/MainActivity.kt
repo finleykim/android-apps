4dfd37f10e17f0b61a1d1847dce324a12e400bd7
김가영 <g_kimmm@naver.com>
2026-09-13 16:14:00 +0900

    [01.BMI-Calculator][FEAT] 정보 입력화면 구현

--- 01.BMI-Calculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ 01.BMI-Calculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.bmicalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bmicalculator.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        val heightEditText: EditText = findViewById(R.id.heightEditText)
+        val weightEditText: EditText = findViewById(R.id.weightEditText)
+        val resultButton: Button = findViewById(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
+                return@setOnClickListener
+            }
+                val height: Int = heightEditText.text.toString().toInt()
+                val weight: Int = weightEditText.text.toString().toInt()
+        }
     }
 }
